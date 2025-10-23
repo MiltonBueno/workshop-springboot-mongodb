65b49846ba807ee480c13d027582eef91af9b176
Milton Bueno <miltonjunior17@gmail.com>
2025-10-23 17:53:43 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/cursojava/workshopmongo/config/Instantiation.java
+++ src/main/java/com/cursojava/workshopmongo/config/Instantiation.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
 import com.cursojava.workshopmongo.domain.Post;
 import com.cursojava.workshopmongo.domain.User;
 import com.cursojava.workshopmongo.dto.AuthorDTO;
+import com.cursojava.workshopmongo.dto.CommentDTO;
 import com.cursojava.workshopmongo.repositories.PostRepository;
 import com.cursojava.workshopmongo.repositories.UserRepository;
 
@@ -43,15 +44,27 @@ public class Instantiation implements CommandLineRunner {
 		                         .atStartOfDay(ZoneId.of("GMT"))
 		                         .toInstant();
 
-		Instant date2 = LocalDate.parse("23/03/2018", formatter)
+		Instant date2 = LocalDate.parse("22/03/2018", formatter)
+		                         .atStartOfDay(ZoneId.of("GMT"))
+		                         .toInstant();
+
+		Instant date3 = LocalDate.parse("23/03/2018", formatter)
 		                         .atStartOfDay(ZoneId.of("GMT"))
 		                         .toInstant();
 
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
 		Post post1 = new Post(null, date1, "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
-		Post post2 = new Post(null, date2, "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		Post post2 = new Post(null, date3, "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", date1, new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", date2, new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", date3, new AuthorDTO(alex));
+		
+		post1.addComment(c1);
+		post1.addComment(c2);
+		post2.addComment(c3);
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
--- src/main/java/com/cursojava/workshopmongo/domain/Post.java
+++ src/main/java/com/cursojava/workshopmongo/domain/Post.java
@@ -2,11 +2,14 @@ package com.cursojava.workshopmongo.domain;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.cursojava.workshopmongo.dto.AuthorDTO;
+import com.cursojava.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -18,6 +21,7 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -69,6 +73,18 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+	
+	public void addComment(CommentDTO comment) {
+		comments.add(comment);
+	}
+	
+	public void removeComment(CommentDTO comment) {
+		comments.remove(comment);
 	}
 
 	@Override
