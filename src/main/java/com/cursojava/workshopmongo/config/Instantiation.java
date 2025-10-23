f56f23d4fe21dcd7d1c25da8ce851207533d8b55
Milton Bueno <miltonjunior17@gmail.com>
2025-10-23 17:36:20 -0300

    Referenciando os posts do usuario

--- src/main/java/com/cursojava/workshopmongo/config/Instantiation.java
+++ src/main/java/com/cursojava/workshopmongo/config/Instantiation.java
@@ -53,6 +53,10 @@ public class Instantiation implements CommandLineRunner {
 		Post post2 = new Post(null, date2, "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
+		
 	}
 
 }
